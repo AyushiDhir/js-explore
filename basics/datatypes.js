e8dc8e69365c0abb74da84782ed4447ced9ade11
AyushiDhir <159688456+AyushiDhir@users.noreply.github.com>
2024-02-11 13:53:06 +0000

    datatypes done

---
+++ basics/datatypes.js
@@ -0,0 +1,27 @@
+"use strict"; //treat all JS code as newer version
+
+//alert(3+3) //wew are using nodejs,not browser
+
+console.log(3+3) 
+console.log("ayushi")
+//code readabilty should be high
+
+let name="ayushi" //string datatype
+
+let age=18
+let isLoggedIn=false
+let state=null
+
+//primitive datatypes
+//number =>2 to power 53
+//bigint
+//string => "",''
+//boolean => true/false
+//null  => standalone value
+// undefined => variable declared but value not assigned
+//symbol =>unique
+
+//object => null
+
+console.log(typeof null) //object
+console.log(typeof undefined) //undefined
